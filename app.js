893bf855521a2e241c2cce93fb604f32a1b9ac5e
Ankur <ankurrajput931567@gmail.com>
2025-08-16 18:30:48 +0530

    your message

--- app.js
+++ app.js
@@ -25,20 +25,52 @@ app.get("/",(req,res)=>{
     res.send("Api is working")
 })
 
+
+data=[
+  {name: "Ankur"},
+  {name: "Ravi"},
+  {name: "Simran"},
+  {name: "Aarti"},
+  {name: "Rahul"},
+  {name: "Priya"},
+  {name: "Neha"},
+  {name: "Karan"},
+  {name: "Pooja"},
+  {name: "Vikas"},
+  {name: "Meena"},
+  {name: "Raj"},
+  {name: "Suman"},
+  {name: "Aman"},
+  {name: "Divya"},
+  {name: "Mohit"},
+  {name: "Komal"},
+  {name: "Sanjay"},
+  {name: "Rekha"},
+  {name: "Nitin"}
+]
+
+
+
 app.get("/data",(req,res)=>{
-    fs.readFile("./data.json","utf8",(err,data)=>{
-        if(err){
-            res.send("error in reading file")
-        }else{
-            try {
-                const parsedData = JSON.parse(data)
-            console.log(parsedData)
-            res.send(parsedData)
-            } catch (error) {
-                res.send("error in parsing")
-            }
-        }
-    })
+    // fs.readFile("./data.json","utf8",(err,data)=>{
+    //     if(err){
+    //         res.send("error in reading file")
+    //     }else{
+    //         try {
+    //             const parsedData = JSON.parse(data)
+    //         console.log(parsedData)
+    //         res.send(parsedData)
+    //         } catch (error) {
+    //             res.send("error in parsing")
+    //         }
+    //     }
+    // })
+    if(data.length==0){
+        res.send("no data found")
+    }
+    else{
+       res.send(data)
+    }
 })
 
 
